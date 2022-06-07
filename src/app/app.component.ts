f32a759b7a61c693de144c60da96464e1a732104
solsolet <gsl21@alu.ua.es>
2022-06-07 18:47:17 +0200

    Aplicación ToDo - Autenticación III: User Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,22 +7,26 @@ import { UserService } from './services/user.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-  title = 'toDo Applicattion for SD';
+  title = 'toDApp para SD';
 
   email: string = 'gsl21@alu.ua.es'; //posar unos q estiguen en la meua bd
   password: string = '1234';
 
-  constructor(public userService: UserService){
+  constructor(public userService: UserService){ }
 
-  }
+  displayName = '';
 
   ngOnInit(){
     this.login();
+    this.userService.getUser().subscribe(
+      user => {this.displayName = user.nombre //nombre per que en api-rest ho tinc aixina
+    });
+    console.log(this.displayName);
   }
 
   login(){
     const user = {email: this.email, password: this.password};
-    console.log(user);
+    //console.log(user);
     this.userService.signIn(user).subscribe(
       (loggedUser) => {
         this.userService.setToken(loggedUser.token);
